cf8215a632a99f2b1dab7d27ce8a8e3a84249c95
HPM digital <137924203+civiliangaming@users.noreply.github.com>
2024-08-26 11:39:54 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function poop() {
+    document.getElementById("tt").style.animation = "bruh 2s infinite";
+    document.getElementById("tt").style.animationPlayState = "running";
+    document.getElementById("hleo").style.animation = "bruh 2s infinite";
+    document.getElementById("hleo").style.animationPlayState = "running";
+    document.getElementById("ggg").style.animation = "bruh1 2s infinite";
+    document.getElementById("ggg").style.animationPlayState = "running";
+    document.getElementById("ggg").style.animationDirection = "reverse";
+    document.getElementById("gg").style.animation = "bruh1 2s infinite";
+    document.getElementById("gg").style.animationPlayState = "running";
+    document.getElementById("gg").style.animationDirection = "reverse";
+}
+
+function diga2() {
+    document.getElementById("tt").style.animationPlayState = "paused";
+    document.getElementById("hleo").style.animationPlayState = "paused";
+    document.getElementById("ggg").style.animationPlayState = "paused";
+    document.getElementById("gg").style.animationPlayState = "paused";
+}
+
